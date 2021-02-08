5522ea98cf0b18334c5ee9b2afcad7fc4ce15cd3
daniikhsan <dani.ikhsan25@gmail.com>
2021-02-08 22:48:16 +0800

    make edit profile

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use((req, res, next) => {
 
     res.locals.userId = req.session.uid;
     res.locals.displayName = req.session.displayName;
+    res.locals.nim = req.session.nim;
     res.locals.moment = moment;
     res.io = socketio;
     next()
